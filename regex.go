fa09d90c4f8a49544b9cabd8be8c5c0b1fe0e293
florent <florent@packetai.co>
2022-01-12 08:57:33 +0100

    First commit

---
+++ regex.go
@@ -0,0 +1,38 @@
+package regex
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type Groups map[string]string
+
+func (g Groups) Get(groupName string) (string, bool) {
+	ret, ok := g[groupName]
+	return ret, ok
+}
+
+func (g Groups) MustGet(groupName string) string {
+	ret, ok := g.Get(groupName)
+	if !ok {
+		panic(fmt.Errorf("Group name: %v not found.", groupName))
+	}
+	return ret
+}
+
+func FindGroups(pattern *regexp.Regexp, s string) Groups {
+	ret := map[string]string{}
+
+	matches := pattern.FindStringSubmatch(s)
+
+	for _, groupName := range pattern.SubexpNames() {
+		index := pattern.SubexpIndex(groupName)
+		if index == -1 {
+			continue
+		}
+		if matches[index] != "" {
+			ret[groupName] = matches[index]
+		}
+	}
+	return ret
+}
---
+++ regex_test.go
@@ -0,0 +1,16 @@
+package regex
+
+import (
+	"regexp"
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+func TestFindGroups(t *testing.T) {
+	source := `(?P<one>\d+) (?P<two>.*)`
+
+	groups := FindGroups(regexp.MustCompile(source), "123 foo")
+	assert.Equal(t, "123", groups.MustGet("one"))
+	assert.Equal(t, "foo", groups.MustGet("two"))
+}
